86fa1567dd8c0680157a649c7c25d24ba6c727af
haydenhoopes <50504383+haydenhoopes@users.noreply.github.com>
2022-01-21 15:50:03 -0700

    worked on lists some more

--- public/js/lists/addItem.js
+++ public/js/lists/addItem.js
@@ -1,28 +1,65 @@
-document.getElementById("disabled-item").addEventListener("input", addItem, { once: true });
+// document.getElementById("disabled-item").addEventListener("input", addItem, { once: true });
 
-function addItem() {
-    let form = document.querySelector("form");
-    let lastItem = form.lastElementChild;
-    let lastText = lastItem.previousElementSibling;
-    var span = document.createElement("span");
-    span.innerHTML = ' X';
-    lastText.appendChild(span);
-
-    document.querySelectorAll("form span").forEach(x => {
-        x.addEventListener("click", removeItem);
+function addXElement() {
+    // Adds an X button to inputs in divs with the input-with-button class
+    document.querySelectorAll(".input-with-button").forEach(div => {
+        let hasButton = false;
+        for (node of div.childNodes) {
+            if (node.tagName == "BUTTON") {
+                hasButton = true;
+                break;
+            }
+        }
+        if (!hasButton) {
+            let b = document.createElement("button");
+            b.innerHTML = "X";
+            b.type = "button";
+            div.appendChild(b);
+        }
     })
-
-    let copy = lastText.cloneNode();
-    console.log(copy);
-    document.getElementById("disabled-item").id = "";
-    copy.id = "disabled-item";
-    copy.value = "";
-    copy.addEventListener("input", addItem, { once: true});
-    form.insertBefore(copy, lastItem);
-    form.insertBefore(span, lastItem);
+    removeItemXEventListener();
 }
 
-function removeItem(e) {
-    e.previousElementSibling.remove();
-    e.remove();
+
+function removeItemXEventListener() {
+    // Adds an event listener to each X button that removes its parent node and everything inside
+    document.querySelectorAll(".input-with-button button").forEach(button => {
+        button.addEventListener("click", function(e) {
+            this.parentElement.remove();
+        })
+    })
 }
+
+
+function addNewLineItem() {
+    // Adds a new div of input when there is content in the input.
+    document.querySelectorAll(".input-with-button").forEach(div => {
+        if (div.querySelector("input").value != "") {
+            if (div.nextElementSibling != null) {
+
+            } else {
+                let newDiv = document.createElement("div");
+                newDiv.classList += "input-with-button";
+                let newInput = document.createElement("input");
+                newInput.classList += "X_removable";
+                newInput.type = "text";
+                newInput.name = "l_Items";
+                newDiv.append(newInput);
+                div.parentElement.append(newDiv);
+                addEventListenerToInput()
+                addXElement();
+            }
+
+        }
+    })
+}
+
+function addEventListenerToInput() {
+    document.querySelectorAll(".input-with-button input").forEach(input => {
+        input.addEventListener("input", addNewLineItem, {once: true});
+    })
+}
+
+// Runs the function once at the start
+addXElement()
+addEventListenerToInput()
